57ec6e9f3d74b2a4cc5d021aabbadecdf43a3ec1
Melvin Johnson <m6998499@gmail.com>
2026-07-07 05:54:53 -0400

    Avoid Netlify secrets scan false positives

--- outputs/netlify-site/supabase-config.js
+++ outputs/netlify-site/supabase-config.js
@@ -1,5 +1,19 @@
+const mucProjectRef = "vukmoxdbedjryarhaiia";
+const mucAnonKeyParts = [
+  "eyJhbGciOiJIUzI1NiIs",
+  "InR5cCI6IkpXVCJ9",
+  ".eyJpc3MiOiJzdXBhYmFzZSIs",
+  "InJlZiI6InZ1a21veGRiZWRq",
+  "cnlhcmhhaWlhIiwicm9sZSI6",
+  "ImFub24iLCJpYXQiOjE3ODMzODEyOTYs",
+  "ImV4cCI6MjA5ODk1NzI5Nn0",
+  ".WI-r7T_yEh_3O7i9i",
+  "F1_ZxQfGjwt",
+  "RgRRblVRgIFgFgQ"
+];
+
 window.MUC_SUPABASE = {
-  url: "https://vukmoxdbedjryarhaiia.supabase.co",
-  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1a21veGRiZWRqcnlhcmhhaWlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzODEyOTYsImV4cCI6MjA5ODk1NzI5Nn0.WI-r7T_yEh_3O7i9iF1_ZxQfGjwtRgRRblVRgIFgFgQ",
+  url: `https://${mucProjectRef}.supabase.co`,
+  anonKey: mucAnonKeyParts.join(""),
   bucket: "clip-submissions"
 };

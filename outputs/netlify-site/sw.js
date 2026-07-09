211455b7eab14f3001a135d91bb3b8863035b750
Melvin Johnson <m6998499@gmail.com>
2026-07-08 20:02:58 -0400

    Use standard upload for free plan videos

--- outputs/netlify-site/sw.js
+++ outputs/netlify-site/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "melanin-unity-clips-netlify-v10";
+const CACHE_NAME = "melanin-unity-clips-netlify-v11";
 const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest", "/icons/icon.svg"];
 
 self.addEventListener("install", (event) => {
